ae6331271bb327a6d0a29f1ca8c4c0ffe964e605
Hamdi-korreshi <hamdikorreshi@gmail.com>
2024-02-23 12:07:39 -0500

    Changed to switch statements since loops did not make sense

--- dfa.c
+++ dfa.c
@@ -20,49 +20,54 @@ int check_phi(char c){
 }
 
 int q8_hk37(char* word, int start, int end){
-    printf("q8 (trap state)\n");
     while( start < end ){
-        printf("%c ", word[start]);
+        printf("Char '%c', started in state q8 and went to q8 (trap state) \n", word[start]);
         start++;
     }
-    return 1;
+    return 8;
 }
 
-int q2_hk37(char *word, int start, int end){
+
+int q3_hk37(char *word, int start, int end){
     if(check_phi(word[start])){
-        return q3_hk37(word, start+1, end);
+        return 3;
     }
     else 
-        return q8_hk37(word, start+1, end);
+        return 8;
 }
 
-int q1_hk37(char *word, int start, int end){
-    while( start < end ){
-        if(check_phi(word[start]))
-            continue;
-        else if (word[start] == period)
-            q0_hk37(word,start+1,end);
-        else if (word[start] == at )
-            q2_hk37(word,start+1,end);
-        else
-        { 
-            q8_hk37(word,start,end);
-            return 1;
-        }
-    }
-    return 0;
-}
-
-int q0_hk37(char *word, int start, int end){
-    printf("q0\n");
+//checks if contuines onto q3 with phi or goes to trap state with anything else
+int q2_hk37(char *word, int start, int end){
     if(check_phi(word[start])){
-        q1_hk37(word,start+1,end);
+        return 3;
     }
-    else{
-        q8_hk37(word,start+1,end);
+    if (word[start] == period)
+        return 4;
+    else
+        return 8;
+}
+
+//checks if contuines onto q2 with @ or goes to q1 with phi or goes to q0 with period or not legal input thus trap state
+int q1_hk37(char *word, int start, int end){
+    if(check_phi(word[start]))
+        return 1;
+    else if (word[start] == period)
+        return 0;
+    else if (word[start] == at )
+        return 2;
+    else
+        return 8;
+}
+
+//checks if contuines onto q1 or goes to trap state
+int q0_hk37(char *word, int start, int end){
+    printf("q0 \n");
+    if(check_phi(word[start])){
         return 1;
     }
-    return 0;
+    else{
+        return 8;
+    }
 }
 
 int main() {
@@ -83,7 +88,49 @@ int main() {
     for (int str=1; str<=num; str++){
         printf("Enter string %d out of %d \n", str, num);
         scanf("%s", buff);
-        printf("String 1: %s\n", buff);
+        printf("String %d: %s\n", str,buff);
+        int length = strlen(buff);
+        int start = 0;
+        printf("length: %d", length);
+        /* 
+            states goes as followed by number ie 0 = q0
+            the line for prev intliazes and checks if it will forever be in trap or go onto q1
+        */
+        int prev = q0_hk37(buff, start, length);
+        printf("Char '%c', started in state q0 and went to q%d\n", buff[start], prev);
+        start++;
+        while (start < length) {
+            switch (prev)
+            {
+                case 0:
+                    printf("Char '%c', started in state q%d", buff[start], prev);
+                    int prev = q0_hk37(buff, start, length);
+                    printf(" and went to q%d\n", prev);
+                    break;\
+                case 1:
+                    printf("Char '%c', started in state q%d", buff[start], prev);
+                    int prev = q1_hk37(buff, start, length);
+                    printf(" and went to q%d\n", prev);
+                    break;
+                case 2:
+                    printf("Char '%c', started in state q%d", buff[start], prev);
+                    int prev = q2_hk37(buff, start, length);
+                    printf(" and went to q%d\n", prev);
+                    break;
+                case 3:
+                    printf("Char '%c', started in state q%d", buff[start], prev);
+                    int prev = q3_hk37(buff, start, length);
+                    printf(" and went to q%d\n", prev);
+                    break;
+                case 8:
+                    printf("Char '%c', started in state q%d and went to q8 (trap state) \n", buff[start], prev);
+                    int prev = q8_hk37(buff, start, length);
+                    break;
+                default:
+                    break;
+            }
+            start++;
+        }
     }
     return 0;
 }

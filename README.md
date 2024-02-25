09be2ecadac79b3dabcf958ae22d728e2633f246
Hamdi-korreshi <hamdikorreshi256@gmail.com>
2024-02-25 14:34:21 -0500

    Completed project and docuementation

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # cs341_email_dfa
 CS341 DFA implementation in code
+Here is the picture of the DFA:
+!dfa_cs341.jpg A DFA for email addresses
+On the sides it explains all the inputs that are possible. 
+
+
+# Thinking and Approach
+At first my thought after making the dfa was to use function class to return things but after realizing that would be a horrible idea I switched my implementation. The idea I got this was from my CS280 class which was a more advanced version of this but that is where I decided to use the switch and scanf was told from the project requirements. As for the reason I use the prev variable and designed the switch states with a function call in the middle, it was based on the fact that I had to show what states switched (example: q1 -> q2). So to save on space I used the same variable and just called in-between the print statements to make it print properly, also made the copy and paste for the case easier. 
+
+As for the check functions, those could've been removed for easier readability, however, for phi realizing it would be called so many times I thought it would be better to make them all functions. Instead of removing them I added comments to make it more readable.
+
+# Execution and Output
+Now that my files were finally built there was one small problem, inputting manually was going to take too much so I used my WSL system to help with that. Here are the steps I took and how you can modify/replicate them.
+
+Step 1, The input file:
+The input file follows a simple procedure, the first line is the number of strings they are going to input, the rest are going to be the email addresses. Look at the input file in the repo for an example
+
+Step 2, Running commands:
+Final to run this file compile it first using (note this is only for the way I ran it):
+
+`gcc -o dfa.c whateverYouWantTheNameToBe`
+
+then change the input how you need it to be and run:
+`./whateverYouWantTheNameToBe < input > output_dfa.txt`
+
+All output should be in the output_dfa.txt file. If you are wondering what the > or < mean, go look up redirect linux with scanf.
--- dfa.c
+++ dfa.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-/* 
+/*
     Hamdi Korreshi
     Section Number: 006
     Semester: Spring 2024
@@ -12,6 +12,7 @@
 char period = '.';
 char at = '@';
 
+// the N described in the dfa
 int check_n(char c){
     if(c== 'v')
         return 7;
@@ -21,6 +22,7 @@ int check_n(char c){
         return 8;
 }
 
+// the delta described in the dfa
 int check_delta(char c){
     if( c == 'r')
         return 1;
@@ -34,15 +36,17 @@ int check_delta(char c){
         return 8;
 }
 
+// the alpha described in the dfa
 int check_alpha(char c){
     if( c == 'g')
         return 1;
-    else if ((c >=97 && c <= 122) && c != 'g')
+    else if (c >=97 && c <= 122)
         return 0;
     else
         return 8;
 }
 
+// the phi described in the dfa
 int check_phi(char c){
     if( c >= 97 && c <= 122)
         return 1;
@@ -50,98 +54,103 @@ int check_phi(char c){
         return 0;
 }
 
+// trap state: prints out every letter if gotten stuck in trap state to enure no illegal address
 int q8_k37(char* word, int start, int end){
     while( start < end ){
         printf("Char '%c', started in state q8 and went to q8 (trap state) \n", word[start]);
         start++;
     }
-    return 8;
+    return end;
 }
 
+// q7 is the final state, however, in this case it is never called if the word is a correct, this state if just more of a check that returns if something like .gov is in the middle
 int q7_k37(char *word, int start, int end){
-    if (check_phi(word[start])){
-        return 7;
+    if (check_phi(word[start])){ // if any letter go back to q3 
+        return 3;
     }
-    else return 0;
+    else if(word[start] == period) // if period got back to q4 just incase it keeps going after that
+        return 4;
+    else 
+        return 8; // else return trap
 }
 
+// q6 is ensuring that the o in gov is working, and redirects to q3 if anyother letter, if period go to q4
 int q6_k37(char *word, int start, int end){
-    if( check_n(word[start]) == 7){
+    if( check_n(word[start]) == 7) // checks if recevied v and goes to q7
         return 7;
-    }
-    else if (check_phi(word[start]) == 3)
+    else if (check_n(word[start]) == 3) // checks if any other letter other then v and returns to q3
         return 3;
-    else if (word[start] == period)
+    else if (word[start] == period) // if period go back to q4
         return 4;
-    else 
-        return 8;
+    else
+        return 8; // otherwise trap state
 }
 
+// q5 ensures that you can move onto q7 with r or q6 with o, any other letter results goes back to q3 (represented by delta), period goes back q4 ensuring no double period is possible
 int q5_k37(char *word, int start, int end){
-    if( check_delta(word[start]) == 1){
+    if( check_delta(word[start]) == 1){ // checks if r, if so go to q7
         return 7;
     }
-    else if (check_delta(word[start]) == 2)
+    else if (check_delta(word[start]) == 2) // checks if o, if so go to q6
         return 6;
-    else if (check_delta(word[start]) == 3)
+    else if (check_delta(word[start]) == 3) // checks if period, if so go to q7
         return 4;
-    else if(check_delta(word[start]) == 0)
+    else if(check_delta(word[start]) == 0) // checks if another letter, if so go to q3
         return 3;
-    else 
-        return 8;
+    else
+        return 8; // otherwise trap
 }
 
+// goes back to q3 if any letter other than g (represented by alpha), if a g move onto q5, otherwise trap state useful for avoiding double periods
 int q4_k37(char *word, int start, int end){
-    if( check_alpha(word[start]) == 1){
+    if(check_alpha(word[start]) == 1){ // gets a g goes to 4
         return 5;
     }
-    else if (check_phi(word[start]) == 0)
+    else if (check_alpha(word[start]) == 0) // gets a letter in  alpha = {a-z}-g
         return 3;
-    else 
-        return 8;
+    else
+        return 8; // anything else
 }
 
+// takes any extra letters waiting for a period (represented by phi), once a period is provided moves onto q4
 int q3_k37(char *word, int start, int end){
-    if(check_phi(word[start])){
+    if(check_phi(word[start])){ // gets a word from phi = {a..z}
         return 3;
     }
-    else if (word[start] == period)
-        return 4;
-    else 
-        return 8;
-}
-
-//checks if contuines onto q3 with phi or goes to trap state with anything else
-int q2_k37(char *word, int start, int end){
-    if(check_phi(word[start])){
-        return 3;
-    }
-    if (word[start] == period)
+    else if (word[start] == period) // if a period
         return 4;
     else
-        return 8;
+        return 8; // other wise trap state
 }
 
-//checks if contuines onto q2 with @ or goes to q1 with phi or goes to q0 with period or not legal input thus trap state
+//checks if continues onto q3 if a letter is provided otherwise trap state
+int q2_k37(char *word, int start, int end){
+    if(check_phi(word[start])){ // gets a word from phi = {a..z} and checks if true
+        return 3;
+    }
+    else
+        return 8; // otherwise trap state
+}
+
+//checks if continues onto q2 with @ or goes to q1 with phi or goes to q0 with period or not legal input thus trap state
 int q1_k37(char *word, int start, int end){
-    if(check_phi(word[start]))
+    if(check_phi(word[start])) // if it is a letter form phi = {a...z} stay at q1
         return 1;
-    else if (word[start] == period)
+    else if (word[start] == period) // if a period go back to q0 just to ensure that a letter must follow after that period
         return 0;
-    else if (word[start] == at )
+    else if (word[start] == at ) // if @ go to q2   
         return 2;
     else
-        return 8;
+        return 8; // otherwise trap
 }
 
-//checks if contuines onto q1 or goes to trap state
+//ensures that a starting letter must be provided at the start of a email or after a period
 int q0_k37(char *word, int start, int end){
-    printf("q0 \n");
-    if(check_phi(word[start])){
-        return 1;
+    if(check_phi(word[start])){ // if a letter in phi = {a..z} go to q1 
+        return 1; 
     }
     else{
-        return 8;
+        return 8; // otherwise trap
     }
 }
 
@@ -160,6 +169,7 @@ int main() {
     }
     char buff[100];
     printf("%d strings to be processed\n", num);
+    printf("---------------------------------------------------------\n\n");
     for (int str=1; str<=num; str++){
         printf("Enter string %d out of %d \n", str, num);
         scanf("%s", buff);
@@ -167,9 +177,9 @@ int main() {
         int length = strlen(buff);
         int start = 0;
         printf("length: %d\n", length);
-        /* 
+        /*
             states goes as followed by number ie 0 = q0
-            the line for prev intliazes and checks if it will forever be in trap or go onto q1
+            the line for prev initializes and checks if it will forever be in trap or go onto q1
         */
         int prev = q0_k37(buff, start, length);
         printf("Char '%c', started in state q0 and went to q%d\n", buff[start], prev);
@@ -219,18 +229,19 @@ int main() {
                     break;
                 case 8:
                     printf("Char '%c', started in state q%d and went to q8 (trap state) \n", buff[start], prev);
-                    prev = q8_k37(buff, start, length);
+                    prev = q8_k37(buff, start+1, length);
                     break;
                 default:
+                    start = length;
                     break;
             }
             start++;
         }
-        if(prev == 8){
-                printf("String %s is not a legal email address\n", buff);
-            }
-            else if (start == length && prev == 7){
-                printf("String %s is a legal email address\n", buff);
+        if(prev == 7)
+            printf("String %s is a legal email address\n", buff);
+        else
+            printf("String %s is not a legal email address\n", buff);
+        printf("---------------------------------------------------------\n\n");
     }
     return 0;
-}}
+}
